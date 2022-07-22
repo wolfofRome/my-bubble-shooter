d50b9a13d597c00a7fba0004a0cf44f5244a052f
wolfofRome <prideofguardian0607@gmail.com>
2022-07-22 15:17:16 +0300

    Setup HexGrid

--- Assets/Scripts/BallCatapult.cs
+++ Assets/Scripts/BallCatapult.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-
 [RequireComponent(typeof(BoxCollider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
 public class BallCatapult : MonoBehaviour
